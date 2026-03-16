be7e4f5946403c5de8849e2e22f61ca5adf82935
ChethanRaj13 <chethanrajjee@gmail.com>
2026-03-16 06:06:45 +0530

    delete method

--- main.py
+++ main.py
@@ -66,5 +66,12 @@ def update_student(student_id: int, student: UpdateStudent):
         students[student_id].test = student.test
 
     return students[student_id]
-    
+
+@app .delete("/delete-student/{student_id}")
+def delete_student(student_id : int):
+    if student_id not in students:
+        return {"Error" : "student doesn't exist"}
+
+    del students[student_id]
+    return {"message" : "deleted successful"}
 
